f7ad9f16e7ba4ccf627d6d4af71a05b97de7dc1a
Steven <steven.d.hondt.sdh@gmail.com>
2022-10-24 05:07:12 +0200

    ✨ Allocating - freeing up memory

--- Week4/copy.c
+++ Week4/copy.c
@@ -10,25 +10,49 @@ int main(void)
 
     string t = s; // Both char *s & char *t point to the same address
 
-    t[0] = toupper(t[0]); // Change source
+    if (strlen(s) > 0) // Check if user entered a string
+    {
+        s[0] = toupper(s[0]); // Change source
+    }
 
     printf("s: %s\n", s);
     printf("t: %s\n", t); // Changes too
 
     s = get_string("Input s: ");
 
-    char *u = malloc(strlen(s) + 1); // Dynamically allocate memory
+    char *u = malloc(strlen(s) + 1); // Dynamically allocate memory (accounting for \0)
+    if (t == NULL)                   // Check if allocation succeeded
+    {
+        printf("Memory coul not be allocated.");
+        return 1;
+    }
+
+    // Copy string
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         u[i] = s[i];
     }
 
     char *v = malloc(strlen(s) + 1);
-    strcpy(v, s); // Shorthand
+    if (t == NULL) // Check if allocation succeeded
+    {
+        printf("Memory coul not be allocated.");
+        return 1;
+    }
 
-    s[0] = toupper(s[0]); // Change source
+    // Copy string - shorthand
+    strcpy(v, s);
+
+    if (strlen(s) > 0) // Check if user entered a string
+    {
+        s[0] = toupper(s[0]); // Change source
+    }
 
     printf("s: %s\n", s);
     printf("u: %s\n", u); // Unchanged
     printf("v: %s\n", v); // Unchanged
+
+    free(u); // Free up the allocated memory !!!
+    free(v);
+    return 0;
 }
