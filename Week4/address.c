ce1d790e04e568bad6b74024622aec8b94b81f84
Steven <steven.d.hondt.sdh@gmail.com>
2022-10-24 04:02:56 +0200

    ✨ Pointers

--- Week4/address.c
+++ Week4/address.c
@@ -6,4 +6,10 @@ int main(void)
     int *p = &n; // pointer declaration - get address
     printf("%p\n", p);
     printf("%i\n", *p); // dereference operator
+
+    char *s = "Hi!";
+    printf("%p\n", s); // address of string (=)
+
+    char *q = &s[0];
+    printf("%p\n", q); // address of first char in string (=)
 }
