17297faeb1d037c6ae587885e413f92d8d3f2e09
Steven <steven.d.hondt.sdh@gmail.com>
2022-10-24 04:32:57 +0200

    ✨ Pointers

--- Week4/compare.c
+++ Week4/compare.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <cs50.h>
 
 int main(void)
@@ -14,4 +15,21 @@ int main(void)
     {
         printf("Different\n");
     }
+
+    if (strcmp(s, t) == 0) // Which is why we use strcmp
+    {
+        printf("Same\n");
+    }
+    else
+    {
+        printf("Different\n");
+    }
+
+    // Entering the same word twice ...
+
+    printf("%s\n", s);
+    printf("%s\n", t); // Same word
+
+    printf("%p\n", s);
+    printf("%p\n", t); // Different address
 }
